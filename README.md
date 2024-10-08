2bf04fb69f65b965e4698c625b065a6b57f34c3e
Troy Neubauer <troyneubauer@gmail.com>
2024-10-07 22:33:33 -0700

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# seify-hackrfone
+
+Rust [Hackrf One](https://greatscottgadgets.com/hackrf/one/) api, with zero native dependencies, powered by [nusb](https://github.com/kevinmehall/nusb)
+
---
+++ examples/info.rs
@@ -0,0 +1,13 @@
+use anyhow::{Context, Result};
+use seify_hackrfone::HackRf;
+
+fn main() -> Result<()> {
+    env_logger::init();
+
+    let radio = HackRf::open_first().context("Failed to open Hackrf")?;
+
+    println!("Board ID: {:?}", radio.board_id());
+    println!("Version: {:?}", radio.version());
+    println!("Device version: {:?}", radio.device_version());
+    Ok(())
+}
